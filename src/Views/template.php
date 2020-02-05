d0869562b18038b7886c09800bc9557e76983fd9
guerlain <gpetitmaire@edenschol.fr>
2020-02-05 14:58:15 +0100

    templated fixed

--- src/Views/template.php
+++ src/Views/template.php
@@ -1,14 +1,18 @@
-<!DOCTYPE html>
-<html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no">
-    <link rel="stylesheet" href="/css/style.css">
-    <title></title>
-  </head>
-  <body>
-    <main>
-      <?php echo $content; ?>
-    </main>
-  </body>
-</html>
+<?php
+
+  ?>
+    <!DOCTYPE html>
+    <html lang="fr" dir="ltr">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, user-scalable=no">
+      <link rel="stylesheet" href="/css/style.css">
+      <title></title>
+    </head>
+    <body>
+      <main>
+        <?php echo $content; ?>
+      </main>
+    </body>
+    </html>
+  <?php
