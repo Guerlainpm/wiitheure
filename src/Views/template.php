194340bce8d1ebe022093957699e99169516b809
guerlain <gpetitmaire@edenschol.fr>
2020-02-05 14:53:33 +0100

    template + wireframe

---
+++ src/Views/template.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <link rel="stylesheet" href="/css/style.css">
+    <title></title>
+  </head>
+  <body>
+    <main>
+      <?php echo $content; ?>
+    </main>
+  </body>
+</html>
