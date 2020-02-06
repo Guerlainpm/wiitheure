2994542584296936b3ee11f10cc8c65ed4ccfd5f
B0UBA <51090351+B0UBA@users.noreply.github.com>
2020-02-06 14:30:58 +0100

    error fixed

---
+++ src/Router.php
@@ -0,0 +1,52 @@
+<?php
+namespace App;
+use App\Controllers\UserController;
+use App\Controllers\WiitController;
+
+class Router {
+    private $method;
+    private $url;
+
+    public function __construct() {
+        $this->method = $_SERVER["REQUEST_METHOD"];
+        $this->url = $_SERVER["REQUEST_URI"];
+    }
+
+    public function run() {
+        $userController = new UserController();
+        $wiitController = new WiitController();
+
+        if ($this->method == "GET") {
+
+            if ($this->url == "/") {
+                $wiitController->index(); 
+            }
+
+            elseif ($this->url == "/authentification") {
+                $userController->showRegisterAndLogin();
+            }
+        }
+       
+        elseif ($this->method == "POST") {
+
+            if (preg_match('#^\/profil\/([0-9]+)$#',$this->url,$match)) {
+                $wiitController->show($match[1]);
+            }
+    
+            elseif ($this->url == "/login") {
+                $userController->login();
+            }
+    
+            elseif ($this->url == "/register") {
+                $userController->register();
+            }
+    
+            elseif ($this->url == "/logout") {
+                $userController->logout();
+            }
+    
+        }
+
+
+    }    
+}
