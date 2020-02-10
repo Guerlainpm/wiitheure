9bd547046bcc4d67293a150ab85a48be93274aba
guerlain <gpetitmaire@edenschol.fr>
2020-02-10 15:02:00 +0100

    authentification form validate

--- src/Views/Auth/authentification.php
+++ src/Views/Auth/authentification.php
@@ -8,11 +8,17 @@
             <form class="flex flex-col p-4 px-12 text-center" action="/login" method="post">
                 <h2 class="mb-8">Se connecter</h2>
                 <label class="mb-2" for="username">Nom d'utilisateur</label>
-                <input class="mb-2 p-2   rounded border border-black" id="login-username" type="text" name="login-username">
+                <input class="mb-2 p-2   rounded border border-black" id="login-username" type="text" name="login-username" value="<?php echo getOld("login-username");?>">
+                <p><?php echo getErrors("login-username"); ?></p>
+
                 <label class="mb-2" for="mail">Mail</label>
-                <input class="mb-2 p-2 rounded border border-black" id="login-mail" type="text" name="login-mail">
+                <input class="mb-2 p-2 rounded border border-black" id="login-mail" type="text" name="login-mail" value="<?php echo getOld("login-mail");?>">
+                <?php echo getErrors("login-mail"); ?>
+
                 <label class="mb-2" for="password">Mot de passe</label>
-                <input class="mb-12 p-2 rounded border border-black" id="login-password" type="password" name="login-password">
+                <input class="mb-12 p-2 rounded border border-black" id="login-password" type="password" name="login-password" value="<?php echo getOld("login-password");?>">
+                <?php echo getErrors("login-password"); ?>
+
                 <button class="p-2 rounded border border-black" type="submit">Envoyer</button>
             </form>
         </div>
@@ -21,13 +27,21 @@
             <form class="flex flex-col p-4 px-12 text-center" action="/register" method="post">
                 <h2 class="mb-8">S'enregistrer</h2>
                 <label class="mb-2" for="username">Nom d'utilisateur</label>
-                <input class="mb-2 p-2  rounded border border-black" id="register-username" type="text" name="register-username">
+                <input class="mb-2 p-2  rounded border border-black" id="register-username" type="text" name="register-username" value="<?php echo getOld("register-username");?>">
+                <?php echo getErrors("register-username"); ?>
+
                 <label class="mb-2" for="mail">Mail</label>
-                <input class="mb-2 p-2  rounded border border-black" id="mail" type="text" name="register-mail">
+                <input class="mb-2 p-2  rounded border border-black" id="mail" type="text" name="register-mail" value="<?php echo getOld("register-mail");?>">
+                <?php echo getErrors("register-mail"); ?>
+
                 <label class="mb-2" for="password">Mot de passe</label>
-                <input class="mb-2 p-2  rounded border border-black" id="password" type="password" name="password">
+                <input class="mb-2 p-2  rounded border border-black" id="password" type="password" name="password" value="<?php echo getOld("password");?>">
+                <?php echo getErrors("password"); ?>
+
                 <label class="mb-2" for="passwordConfirm">Confirmer le mot de passe</label>
-                <input class="mb-12 p-2 rounded border border-black" id="passwordConfirm" type="password" name="passwordConfirm">
+                <input class="mb-12 p-2 rounded border border-black" id="passwordConfirm" type="password" name="passwordConfirm" value="<?php echo getOld("passwordConfirm");?>">
+                <?php echo getErrors("passwordConfirm"); ?>
+
                 <button class="p-2 rounded border border-black" type="submit">Envoyer</button>
             </form>
         </div>
@@ -35,5 +49,6 @@
 </main>
 <?php
 $content = ob_get_clean();
-
+unset($_SESSION["errors"]);
+unset($_SESSION["old"]);
 require VIEWS . '/template.php';
