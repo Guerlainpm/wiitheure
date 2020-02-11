11cd52435ba14328e3808d5497af02ab29bbe651
guerlain <gpetitmaire@edenschol.fr>
2020-02-11 10:26:09 +0100

    footer

--- src/Views/components/navbar.php
+++ src/Views/components/navbar.php
@@ -3,19 +3,26 @@ ob_start();
 ?>
 <div class="text-white bg-primary p-2 md:flex md:items-center md:justify-between md:px-40">
   <div class="container mx-auto flex justify-between items-center">
-    <div class="w-32 rounded-full bg-white h-full">
+    <div class="w-24 rounded-full bg-white h-full">
       <img src="/logo.png" alt="logo">
     </div>
-      <input class="border-2 border-grey rounded-full outline-none py-1 px-2 w-64" type="search" placeholder="Rechercher" name="search">
+      <input class="text-dark border-2 border-grey rounded-full outline-none py-2 px-4 w-1/3" type="search" placeholder="Rechercher" name="search">
       <?php
         if (isset($_SESSION["user"])) {
           ?>
-            <div class="h-20 justify-between flex flex-col items-center">
+            <div class="w-64 h-20 justify-between flex flex-col items-center">
               <h2 class="text-white">Connecter en tant que <?php echo $_SESSION["user"]->getUsername(); ?></h2>
               <form class="w-full" action="/logout" method="post">
                 <button class=" text-blue-500 bg-white w-full border-2 border-blue-600 rounded-full uppercase py-1 px-2 transition duration-500 hover:bg-primary hover:text-white hover:border-white" type="submit" name="button">Logout</button>
               </form>
             </div>
+          <?php
+        }
+        else {
+          ?>
+          <div class="w-64 h-20 flex items-center">
+            <a href="/authentification" class="text-center text-blue-500 bg-white w-full border-2 border-blue-600 rounded-full uppercase py-1 px-2 transition duration-500 hover:bg-primary hover:text-white hover:border-white" type="submit" name="button">Login</a>
+          </div>
           <?php
         }
       ?>
