3740588908d740e28430a9698948b908bef5328f
guerlain <gpetitmaire@edenschol.fr>
2020-02-17 17:25:25 +0100

    index front fixed

--- src/Views/Auth/profile.php
+++ src/Views/Auth/profile.php
@@ -1,13 +1,11 @@
 <?php
    ob_start();
-?>
 
+?>
   <div class="flex flex-wrap justify-center">
     <div class="md:w-1/3 w-2/3 h-screen overflow-y-auto border-r-2 border-dark flex-col md:flex hidden" id="profil-<?php echo $_SESSION["user"]->getId();?>">
       <h2 class="text-3xl text-light"><?php echo $_SESSION["user"]->getUsername(); ?></h2>
       <p>Créé le : <?php echo $_SESSION['user']->getCreate_at(); ?></p>
-
-
       <div class="w-full p-4 flex flex-col">
         <div class="w-full flex flex-col py-4 items-center">
           <div class="w-full flex flex-col flex-grow py-4">
@@ -27,12 +25,16 @@
 
               <div id="bio-<?php echo $data["user"]->getId();?>" class="mb-8">
                 <p class="text-light border-b-2 border-dark">Biographie</p>
+
                 <p class="ml-4 pt-2"><?php echo $data['user']->getBio(); ?></p>
+
                 <p class="ml-4 text-red-500"><?php echo getErrors('bio'); ?></p>
 
               </div>
           <!-- form -->
+
               <form class="hidden flex-col justify-around" id="update-<?php echo$data["user"]->getId();?>" action="/profile/<?php echo $_SESSION['user']->getId(); ?>/edit" method="post">
+
                   <div class="flex flex-col mb-8">
                     <label class="text-light border-b-2 border-dark" for="username">Pseudo</label>
                     <input class="outline-none mt-2 rounded-lg p-4" type="text" name="username" value="<?php echo $data["user"]->getUsername();?>" placeholder="Username">
@@ -41,13 +43,15 @@
 
                   <div class="flex flex-col mb-8">
                     <label class="text-light border-b-2 border-dark" for="username">E-mail</label>
+
                     <input class="outline-none mt-2 rounded-lg p-4" type="text" name="mail" value="<?php echo $data['user']->getMail(); ?>" placeholder="Mail">
+
                     <p class="ml-4 text-red-500"><?php echo getErrors('mail'); ?></p>
                   </div>
 
                   <div class="flex flex-col mb-8">
                     <label class="text-light border-b-2 border-dark" for="username">Biographie</label>
-                    <textarea class="outline-none mt-2 resize-none rounded-lg p-4" name="bio" rows="8" cols="80" placeholder="Bio"><?php echo $_SESSION['user']->getBio(); ?></textarea>
+                    <textarea class="outline-none mt-2 resize-none rounded-lg p-4" name="bio" rows="8" cols="80" placeholder="Bio"><?php echo $_SESSION["user"]->getBio(); ?></textarea>
                     <p class="ml-4 text-red-500"><?php echo getErrors('bio'); ?></p>
                   </div>
                   <div class="flex justify-center mb-2">
@@ -56,7 +60,9 @@
                 </form>
 
                 <div class="flex justify-center">
+
                   <button class="outline-none h-10 w-10 rounded-full bg-dark text-white transition duration-100 hover:bg-light" onclick="toggle(<?php echo $_SESSION['user']->getId();?>)" id="toggleUser-<?php echo $_SESSION['user']->getId();?>"><i class="fas fa-pencil-alt"></i></button>
+
                 </div>
 
               </div>
@@ -84,7 +90,9 @@
         <?php foreach ($data['wiit'] as $wiit) {
           ?>
           <div class="bg-white p-4 rounded-lg border-2 border-light_2 h-48 mb-8 flex flex-col justify-between">
+
               <p class="mb-2 text-light border-b-2 border-dark"><?php echo $data['user']->getUsername(); ?></p>
+
               <div class="ml-8 text-sm overflow-y-auto max-h-full">
                 <?php echo $wiit["content"]; ?>
               </div>
--- src/Views/components/navbar.php
+++ src/Views/components/navbar.php
@@ -6,29 +6,22 @@ ob_start();
     <a href="/" class="w-20 md:w-24 lg:w-20 rounded-full bg-white h-full">
       <img src="/logo.png" alt="logo">
     </a>
-      <form action="/search" method="post">
-        <label for="search" class="text-white">search</label>
-        <div class="flex">
-          <input id="search" class="text-black bg-white w-full border-t-2 border-b-2 border-l-2 border-blue-600 rounded-l-lg py-1 px-2" name="search"/>
-          <button class="text-blue-500 bg-white px-2 rounded-r-lg border-t-2 border-b-2 border-r-2 border-blue-600" type="submit"><i class="fas fa-search"></i></button>
-        </div>
-      </form>
       <?php
         if (isset($_SESSION["user"])) {
           ?>
+          <form action="/search" method="post">
+            <label for="search" class="text-white">search</label>
+            <div class="flex">
+              <input id="search" class="text-black bg-white w-full border-t-2 border-b-2 border-l-2 border-blue-600 rounded-l-lg py-1 px-2" name="search"/>
+              <button class="text-blue-500 bg-white px-2 rounded-r-lg border-t-2 border-b-2 border-r-2 border-blue-600" type="submit"><i class="fas fa-search"></i></button>
+            </div>
+          </form>
             <div class="w-64 h-20 justify-between flex flex-col items-center">
-              <h2 class="flex flex-wrap items-center text-white">Connected as : <a href="/profile/<?php echo $_SESSION['user']->getId(); ?>" class="ml-2 underline"><?php echo $_SESSION["user"]->getUsername(); ?></a></h2>
+              <h2 class="flex flex-wrap items-center text-white">Connecter en tant que: <a href="/profile/<?php echo $_SESSION['user']->getId(); ?>" class="ml-2 underline"><?php echo $_SESSION["user"]->getUsername(); ?></a></h2>
               <form class="w-full" action="/logout" method="post">
-                <button class=" text-blue-500 bg-white w-full border-2 border-blue-600 rounded-lg uppercase py-1 px-2 transition duration-500 hover:bg-blue-600 hover:text-white hover:border-white" type="submit" name="button">Logout</button>
+                <button class=" text-blue-500 bg-white w-full border-2 border-blue-600 rounded-lg uppercase py-1 px-2 transition duration-500 hover:bg-blue-600 hover:text-white hover:border-white" type="submit" name="button">Se déconnecter</button>
               </form>
             </div>
-          <?php
-        }
-        else {
-          ?>
-          <div class="lg:w-64 w-24 h-20 flex items-center">
-            <a href="/authentification" class="text-center text-blue-500 bg-white w-full border-2 border-blue-600 rounded-full uppercase py-1 px-2 transition duration-500 hover:bg-primary hover:text-white hover:border-white" type="submit" name="button">Login</a>
-          </div>
           <?php
         }
       ?>
